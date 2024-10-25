0d31bc489f25989343e8f1553040f980765c15ff
juljimgar <julianj1202@gmail.com>
2024-10-25 17:25:43 +0200

    Commit segundo

--- app/src/main/java/com/iesantoniosequeros/componentesbasicos/MainActivity.kt
+++ app/src/main/java/com/iesantoniosequeros/componentesbasicos/MainActivity.kt
@@ -17,29 +17,35 @@ class MainActivity : AppCompatActivity() {
         jjg = 0
         android.util.Log.i("ESTADOS","Creando la actividad:"+contador)
         Log.i("ESTADOS","Evento onCreate:"+contador)
-        Log.i("ESTADOS","Evento onCreate:"+jjg)
+        Log.i("ESTADOS","Evento onCreate [JJG]:"+jjg)
         setContentView(R.layout.activity_main)
     }
     override fun onStart() {
         super.onStart()
-        contador++;
+        contador++
+        jjg++
         Log.i("ESTADOS","Evento onStart:"+contador)
+        Log.i("ESTADOS","Evento onStart [JJG]:"+jjg)
     }
     override fun onResume() {
         super.onResume()
         contador++;
+        jjg++
         Log.i("ESTADOS","Evento onResume:"+contador)
-
+        Log.i("ESTADOS","Evento onResume [JJG]:"+jjg)
     }
     override fun onStop() {
         super.onStop()
         contador++;
+        jjg++
         Log.i("ESTADOS","Evento onStop:"+contador)
+        Log.i("ESTADOS","Evento onStop [JJG]:"+jjg)
     }
     override fun onDestroy() {
         super.onDestroy()
+        jjg++
         Log.i("ESTADOS","Evento onDestroy:"+contador)
-
+        Log.i("ESTADOS","Evento onDestroy [JJG]:"+jjg)
 
     }
 
