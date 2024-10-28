639c34eee58cca78127eef5658a420280ab799ef
juljimgar <julianj1202@gmail.com>
2024-10-28 19:39:36 +0100

    Commit tercero

--- app/src/main/java/com/iesantoniosequeros/componentesbasicos/JJG_PrimosActivity.kt
+++ app/src/main/java/com/iesantoniosequeros/componentesbasicos/JJG_PrimosActivity.kt
@@ -1,6 +1,8 @@
 package com.iesantoniosequeros.componentesbasicos
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,12 +13,49 @@ class JJG_PrimosActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_jjg_primos)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val pulsar = this.findViewById<Button>(R.id.button1)
+        val entrada = this.findViewById<TextView>(R.id.textView2)
+        val resultado = this.findViewById<TextView>(R.id.textView1)
 
+        fun isPrime(n: Int): Boolean {
+            if (n < 2) return false
+            for (i in 2..Math.sqrt(n.toDouble()).toInt()) {
+                if (n % i == 0) {
+                    return false
+                }
+            }
+            return true
+        }
 
+        fun searchPrimesRecursive(n: Int): List<Int> {
+            if (n <= 1) return emptyList()
+            val primes = searchPrimesRecursive(n - 1).toMutableList()
+            if (isPrime(n)) primes.add(n)
+            return primes
+        }
+
+        pulsar.setOnClickListener {
+            val inputText = entrada.text.toString()
+            val n = inputText.toInt()
+
+            val primes = searchPrimesRecursive(n - 1)
+
+            resultado.text = primes.toString()
+        }
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+
+            }
         }
     }
-}
+
+
+
+
+
+
+
+
--- app/src/main/java/com/iesantoniosequeros/componentesbasicos/MainActivity.kt
+++ app/src/main/java/com/iesantoniosequeros/componentesbasicos/MainActivity.kt
@@ -1,53 +1,72 @@
 package com.iesantoniosequeros.componentesbasicos
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    var contador:Int=0;
+    var contador: Int = 0;
     var jjg = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         contador++;
         jjg = 0
-        android.util.Log.i("ESTADOS","Creando la actividad:"+contador)
-        Log.i("ESTADOS","Evento onCreate:"+contador)
-        Log.i("ESTADOS","Evento onCreate [JJG]:"+jjg)
+        android.util.Log.i("ESTADOS", "Creando la actividad:" + contador)
+        Log.i("ESTADOS", "Evento onCreate:" + contador)
+        Log.i("ESTADOS", "Evento onCreate [JJG]:" + jjg)
         setContentView(R.layout.activity_main)
+
+
+        var b_estados=findViewById<Button>(R.id.button2)
+        b_estados.setOnClickListener() {
+            val intent: Intent = Intent(this, JJG_PrimosActivity::class.java)
+            startActivityForResult(intent,1)
+        }
     }
+
+
+
     override fun onStart() {
         super.onStart()
         contador++
         jjg++
-        Log.i("ESTADOS","Evento onStart:"+contador)
-        Log.i("ESTADOS","Evento onStart [JJG]:"+jjg)
+        Log.i("ESTADOS", "Evento onStart:" + contador)
+        Log.i("ESTADOS", "Evento onStart [JJG]:" + jjg)
     }
+
     override fun onResume() {
         super.onResume()
         contador++;
         jjg++
-        Log.i("ESTADOS","Evento onResume:"+contador)
-        Log.i("ESTADOS","Evento onResume [JJG]:"+jjg)
+        Log.i("ESTADOS", "Evento onResume:" + contador)
+        Log.i("ESTADOS", "Evento onResume [JJG]:" + jjg)
     }
+
     override fun onStop() {
         super.onStop()
         contador++;
         jjg++
-        Log.i("ESTADOS","Evento onStop:"+contador)
-        Log.i("ESTADOS","Evento onStop [JJG]:"+jjg)
+        Log.i("ESTADOS", "Evento onStop:" + contador)
+        Log.i("ESTADOS", "Evento onStop [JJG]:" + jjg)
     }
+
     override fun onDestroy() {
         super.onDestroy()
         jjg++
-        Log.i("ESTADOS","Evento onDestroy:"+contador)
-        Log.i("ESTADOS","Evento onDestroy [JJG]:"+jjg)
+        Log.i("ESTADOS", "Evento onDestroy:" + contador)
+        Log.i("ESTADOS", "Evento onDestroy [JJG]:" + jjg)
 
     }
+
+
+
+
 
 
 }
